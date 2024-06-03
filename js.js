1463fc80d7cf9c886881da887af00eb4c51a1296
wbbi <161730639+REKISI@users.noreply.github.com>
2024-06-03 13:51:32 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function sendMessage() {
+             const Nama = document.getElementById("Nama Lengkap");
+             const Email = document.getElementById("Email");
+             const Pesan = document.getElementById("Pesan");
+
+}
+
+h1 {
+                    document.textContentmarquee(mar)
+}
+ 
+
